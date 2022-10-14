ec9084365633424b96d1041d59cf64804c8a2b02
Ansab-Sajjad <ansab@troontechnologies.com>
2022-10-14 12:42:09 +0500

    Initial Commit

--- cypress/e2e/Care Hubble/Provider/Provider_Main.cy.js
+++ cypress/e2e/Care Hubble/Provider/Provider_Main.cy.js
@@ -5,6 +5,8 @@ import Provider_Check_out from "./Provider_Checkout.cy"
 import Extend_Visit from "./Extend_Visit.cy"
 import Early_Checkout from "./Early_checkout.cy"
 import provider_Check_In_With_Test_Cases from "./Provider_CheckIn_Nagitive_Test Cases.cy"
+import Reshedule from "./Rescadule.cy"
+import Direct_Cancel_Visit from "./Direct_Cancel_Visit.cy"
 
 
 
@@ -13,8 +15,7 @@ describe("Let's automate the Care Hubble.", function () {
 
     //    <==================== Provider Login ====================>
 
-    Provider_Login()
-
+    Provider_Login()  // => Must ON.
 
     //    <==================== Provider Profile SetUp ====================>
 
@@ -22,20 +23,29 @@ describe("Let's automate the Care Hubble.", function () {
 
     //    <==================== Provider Check_In ====================>
     
-    provider_Check_In()
-
+    // provider_Check_In() 
+    
     // provider_Check_In_With_Test_Cases()
 
     //    <==================== Provider Check_Out ====================>
 
-    //  Provider_Check_out() 
+    //  Provider_Check_out() //=> On for Extend & Early Check out.
 
     //    <==================== Extend Visit ====================>
 
-    Extend_Visit()
+    // Extend_Visit()
 
     //    <==================== Early Checkout ====================>
 
-    Early_Checkout()
+    // Early_Checkout()
+
+    //    <==================== Reschedule ====================>
+
+    Reshedule()
+
+    //    <==================== Direct Cancel Care Visit ====================>
+
+    // Direct_Cancel_Visit() 
+
 
 })
